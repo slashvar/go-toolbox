aa6f8341291069010ac3e35e34037dd9d97822eb
Marwan Burelle <marwan.burelle@algolia.com>
2022-05-27 16:57:52 +0200

    feat: more functions

--- utils/slices.go
+++ utils/slices.go
@@ -1,6 +1,8 @@
 package utils
 
-import "golang.org/x/exp/constraints"
+import (
+	"golang.org/x/exp/constraints"
+)
 
 // Map converts a slice of InType into a slice of OutType using function f
 func Map[InType, OutType any](in []InType, f func(InType) OutType) []OutType {
@@ -89,6 +91,7 @@ func Insert[T any](x T, pos int, s []T) []T {
 }
 
 // LowerBound returns the position of the first elements that is not smaller than x
+// LowerBound returns a coherent result only if s is sorted
 func LowerBound[T constraints.Ordered](x T, s []T) int {
 	left, right := 0, len(s)
 	for left < right {
@@ -103,4 +106,40 @@ func LowerBound[T constraints.Ordered](x T, s []T) int {
 		left = mid + 1
 	}
 	return left
+}
+
+// Partition moves elements to form a partition and returns the position of the pivot
+// Let p := Partition(x, s) then for all i < p s[i] < x and for all i >= p, s[i] >= x
+func Partition[T constraints.Ordered](x T, s []T) int {
+	pivot := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] < x {
+			s[i], s[pivot] = s[pivot], s[i]
+			pivot++
+		}
+	}
+	return pivot
+}
+
+// PartitionFilter moves elements to form a partition w.r.t to f
+// Let p := PartitionFilter(f, s) then for all i < p, f([i]) is true and for all i >= p f(s[i]) is false
+func PartitionFilter[T any](f func(T) bool, s []T) int {
+	pivot := 0
+	for i := 0; i < len(s); i++ {
+		if f(s[i]) {
+			s[i], s[pivot] = s[pivot], s[i]
+			pivot++
+		}
+	}
+	return pivot
+}
+
+// Combine combines elements of s1 and s2 using zipper function
+// returned slice of the length of the smallest input
+func Combine[T1, T2, T3 any](s1 []T1, s2 []T2, zipper func(T1, T2) T3) []T3 {
+	var r []T3
+	for i := 0; i < len(s1) && i < len(s2); i++ {
+		r = append(r, zipper(s1[i], s2[i]))
+	}
+	return r
 }
--- utils/slices_test.go
+++ utils/slices_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -184,46 +185,6 @@ func TestFilter(t *testing.T) {
 	}
 }
 
-func TestReverse(t *testing.T) {
-	equalRev := func(s1, s2 []int) bool {
-		if len(s1) != len(s2) {
-			return false
-		}
-		for i := range s1 {
-			if s1[i] != s2[len(s2)-i-1] {
-				return false
-			}
-		}
-		return true
-	}
-	type testCase struct {
-		name string
-		in   []int
-	}
-	cases := []testCase{
-		{
-			name: "reverse empty slice",
-			in:   []int{},
-		},
-		{
-			name: "reverse even length slice",
-			in:   buildSlice(10, 1),
-		},
-		{
-			name: "reverse odd length slice",
-			in:   buildSlice(11, 1),
-		},
-	}
-	for _, tt := range cases {
-		t.Run(tt.name, func(t *testing.T) {
-			r := make([]int, len(tt.in))
-			copy(r, tt.in)
-			Reverse(r)
-			require.True(t, equalRev(tt.in, r))
-		})
-	}
-}
-
 func TestOptionalFilter(t *testing.T) {
 	type testCases struct {
 		input  []int
@@ -266,6 +227,46 @@ func TestOptionalFilter(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			r := OptionalFilter(tt.f, tt.input)
 			require.True(t, eq(tt.output, r))
+		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	equalRev := func(s1, s2 []int) bool {
+		if len(s1) != len(s2) {
+			return false
+		}
+		for i := range s1 {
+			if s1[i] != s2[len(s2)-i-1] {
+				return false
+			}
+		}
+		return true
+	}
+	type testCase struct {
+		name string
+		in   []int
+	}
+	cases := []testCase{
+		{
+			name: "reverse empty slice",
+			in:   []int{},
+		},
+		{
+			name: "reverse even length slice",
+			in:   buildSlice(10, 1),
+		},
+		{
+			name: "reverse odd length slice",
+			in:   buildSlice(11, 1),
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := make([]int, len(tt.in))
+			copy(r, tt.in)
+			Reverse(r)
+			require.True(t, equalRev(tt.in, r))
 		})
 	}
 }
@@ -318,6 +319,208 @@ func TestInsert(t *testing.T) {
 				return
 			}
 			require.Equal(t, tt.value, r[len(r)-1])
+		})
+	}
+}
+
+func TestLowerBound(t *testing.T) {
+	type testCase struct {
+		name     string
+		in       []int
+		value    int
+		expected int
+	}
+	cases := []testCase{
+		{
+			name:     "empty slice",
+			in:       []int{},
+			value:    0,
+			expected: 0,
+		},
+		{
+			name:     "find first",
+			in:       buildSlice(10, 1),
+			value:    0,
+			expected: 0,
+		},
+		{
+			name:     "find last",
+			in:       buildSlice(10, 1),
+			value:    9,
+			expected: 9,
+		},
+		{
+			name:     "find mid",
+			in:       buildSlice(10, 1),
+			value:    5,
+			expected: 5,
+		},
+		{
+			name:     "not present, lower bound in the middle",
+			in:       buildSlice(10, 2),
+			value:    5,
+			expected: 3,
+		},
+		{
+			name:     "not present, lower bound first",
+			in:       buildSlice(10, 2),
+			value:    -1,
+			expected: 0,
+		},
+		{
+			name:     "not present, lower bound out-of-range",
+			in:       buildSlice(10, 1),
+			value:    10,
+			expected: 10,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			p := LowerBound(tt.value, tt.in)
+			require.Equal(t, tt.expected, p)
+		})
+	}
+}
+
+func shuffleSlice(length, step int) []int {
+	s := buildSlice(length, step)
+	rand.Shuffle(len(s), func(i, j int) {
+		s[i], s[j] = s[j], s[i]
+	})
+	return s
+}
+
+func TestPartition(t *testing.T) {
+	type testCase struct {
+		name  string
+		in    []int
+		value int
+	}
+	cases := []testCase{
+		{
+			name:  "empty slice",
+			in:    []int{},
+			value: 0,
+		},
+		{
+			name:  "partition sorted slice",
+			in:    buildSlice(10, 1),
+			value: 5,
+		},
+		{
+			name:  "partition shuffled slice",
+			in:    shuffleSlice(10, 1),
+			value: 5,
+		},
+		{
+			name:  "partition shuffled slice with value before range",
+			in:    shuffleSlice(10, 1),
+			value: -1,
+		},
+		{
+			name:  "partition shuffled slice with value after range",
+			in:    shuffleSlice(10, 1),
+			value: 20,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Partition(tt.value, tt.in)
+			require.LessOrEqual(t, p, len(tt.in))
+			for i := 0; i < p; i++ {
+				require.Less(t, tt.in[i], tt.value)
+			}
+			for i := p; i < len(tt.in); i++ {
+				require.GreaterOrEqual(t, tt.in[i], tt.value)
+			}
+		})
+	}
+}
+
+func TestPartitionFilter(t *testing.T) {
+	type testCase struct {
+		name      string
+		in        []int
+		predicate func(int) bool
+	}
+	cases := []testCase{
+		{
+			name:      "empty slice",
+			in:        []int{},
+			predicate: func(int) bool { return true },
+		},
+		{
+			name:      "even and odd partition",
+			in:        buildSlice(10, 1),
+			predicate: func(n int) bool { return n%2 == 0 },
+		},
+		{
+			name:      "even and odd partition, only even elements",
+			in:        buildSlice(10, 2),
+			predicate: func(n int) bool { return n%2 == 0 },
+		},
+		{
+			name:      "partition with predicate always false",
+			in:        buildSlice(10, 2),
+			predicate: func(int) bool { return false },
+		},
+		{
+			name:      "partition with predicate always true",
+			in:        buildSlice(10, 2),
+			predicate: func(int) bool { return true },
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			p := PartitionFilter(tt.predicate, tt.in)
+			require.LessOrEqual(t, p, len(tt.in))
+			for _, e := range tt.in[:p] {
+				require.True(t, tt.predicate(e))
+			}
+			for _, e := range tt.in[p:] {
+				require.False(t, tt.predicate(e))
+			}
+		})
+	}
+}
+
+func TestCombine(t *testing.T) {
+	zipper := func(a, b int) int { return a + b }
+	type testCase struct {
+		name string
+		in1  []int
+		in2  []int
+	}
+	cases := []testCase{
+		{
+			name: "empty slices",
+			in1:  []int{},
+			in2:  []int{},
+		},
+		{
+			name: "same length",
+			in1:  buildSlice(10, 1),
+			in2:  buildSlice(10, 1),
+		},
+		{
+			name: "in1 smaller",
+			in1:  buildSlice(5, 1),
+			in2:  buildSlice(10, 1),
+		},
+		{
+			name: "in1 longer",
+			in1:  buildSlice(10, 1),
+			in2:  buildSlice(5, 1),
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := Combine(tt.in1, tt.in2, zipper)
+			require.LessOrEqual(t, len(r), len(tt.in1))
+			require.LessOrEqual(t, len(r), len(tt.in2))
+			for i := 0; i < len(r); i++ {
+				require.Equal(t, tt.in1[i]+tt.in2[i], r[i])
+			}
 		})
 	}
 }
